14d58feda8f7420bdb9aa4b5b32ecb18673d15cf
Abdurrahman Oyediran <oyediranabdurrahman@gmail.com>
2024-08-30 19:05:25 -0500

    filteration

--- project.js
+++ project.js
@@ -1,38 +1,45 @@
 const projects = ` [
     {
+      "link": "https://coderahman.github.io/portfolio/",
       "name": "Portfolio Website",
       "category": "HTML, CSS, JS",
       "img": "images/dispic6.png"
     },
     {
+      "link": "https://coderahman.github.io/tasklist/",
       "name": "Task List",
-      "category": "HTML, CSS",
-      "img": "images/dispic1.png"
+      "category": "HTML, CSS, JS",
+      "img": "images/dispic1.jpeg"
     },
     {
+      "link": "https://coderahman.github.io/memegeneratorchallenge/",
       "name": "Meme Genrator",
-      "category": "HTML, CSS",
-      "img": "images/dispic2.png"
+      "category": "HTML, CSS, JS",
+      "img": "images/dispic2.jpeg"
     },
     {
+      "link": "https://coderahman.github.io/sociallinkspage/",
       "name": "Social Links",
       "category": "HTML, CSS",
-      "img": "images/dispic3.png"
+      "img": "images/dispic3.jpeg"
     },
     {
+      "link": "https://coderahman.github.io/blogpreviewcard/",
       "name": "Blog Preview",
       "category": "HTML, CSS",
       "img": "images/dispic5.png"
     },
     {
+      "link": "https://coderahman.github.io/singlepricecomp/",
       "name": "Single Price",
-      "category": "HTML, CSS",
+      "category": "HTML, CSS, JS",
       "img": "images/dispic7.png"
     }
   ]
   `
   const template = `
   {
+  "link": "",
   "name": "",
   "category": "",
   "img": ""
--- scripts.js
+++ scripts.js
@@ -11,28 +11,22 @@ const projects = document.getElementById("projects");
 
 function addProjects(projects) {
 
-    // loop over each item in the data
+    
     for (let i = 0; i < projects.length; i++) {
         const project = projects[i];
-    
 
-        // create a new div element, which will become the game card
         const projectCard = document.createElement('div');
 
-        // add the class game-card to the list
         projectCard.classList.add('project-card');
 
-        // set the inner HTML using a template literal to display some info 
-        // about each game
-        // TIP: if your images are not displaying, make sure there is space
-        // between the end of the src attribute and the end of the tag ("/>")
-
         projectCard.innerHTML = `
-        <img src="${project.img}" width="100%" class="projimg" alt="${project.name} image">
-        <div class="category"><h4>${project.category}</h4></div>
-        <h3 class="name">${project.name}</h3>
+        <a href="${project.link}" class="projectlink" target="_blank">
+            <img src="${project.img}" width="100%" class="projimg" alt="${project.name} image">
+            <div class="category"><h4>${project.category}</h4></div>
+            <h3 class="name">${project.name}</h3>
+        </a>
     `;
-        // append the game to the games-container
+        
         const projectsContainer = document.getElementById('projects');
         projectsContainer.appendChild(projectCard);
 
@@ -46,29 +40,29 @@ addProjects(PROJECT_JSON)
 function filterhtmlcssOnly() {
     deleteChildElements(projects);
 
-    // use filter() to get a list of games that have not yet met their goal
+    
     const htmlcssproj = PROJECT_JSON.filter(project => project.category == "HTML, CSS");
     console.log(htmlcssproj.length);
 
-    // use the function we previously created to add the unfunded games to the DOM
+    
     addProjects(htmlcssproj);
 }
 
 function filterhtmlcssjs() {
     deleteChildElements(projects);
 
-    // use filter() to get a list of games that have not yet met their goal
+    
     const htmlcssjsproj = PROJECT_JSON.filter(project => project.category == "HTML, CSS, JS");
     console.log(htmlcssjsproj.length);
 
-    // use the function we previously created to add the unfunded games to the DOM
+    
     addProjects(htmlcssjsproj);
 }
 
 function showAllProj() {
     deleteChildElements(projects);
 
-    // add all games from the JSON data to the DOM
+    
     addProjects(PROJECT_JSON);
 }
 
@@ -76,7 +70,7 @@ const htmlcssBtn = document.getElementById("html-css");
 const htmlscssjsBtn = document.getElementById("html-css-js");
 const allBtn = document.getElementById("all-btn");
 
-// add event listeners with the correct functions to each button
+
 htmlcssBtn.addEventListener("click", filterhtmlcssOnly);
 htmlscssjsBtn.addEventListener("click", filterhtmlcssjs);
 allBtn.addEventListener("click", showAllProj);
