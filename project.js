3965ea49cca57a3eb8e1e861680973c71e70a077
Abdurrahman Oyediran <oyediranabdurrahman@gmail.com>
2025-11-28 17:56:52 -0600

    changes

--- project.js
+++ project.js
@@ -1,21 +1,21 @@
 const projects = ` [
     {
-      "link": "https://abdurrahmanoyediran.netlify.app",
-      "name": "Portfolio Website",
-      "category": "HTML, CSS, JS",
-      "img": "images/dispic6.png"
+      "link": "https://nasaastronomypictures.netlify.app/",
+      "name": "NASA Astronomy Pictures",
+      "category": "REACT",
+      "img": "images/dispic11.png"
+    },
+    {
+      "link": "https://southernmissgames.netlify.app/",
+      "name": "Southern Miss Games",
+      "category": "REACT",
+      "img": "images/dispic9.png"
     },
     {
       "link": "https://coderahman.github.io/tasklist/",
       "name": "Task List",
       "category": "HTML, CSS, JS",
       "img": "images/dispic1.jpeg"
-    },
-    {
-      "link": "https://coderahman.github.io/memegeneratorchallenge/",
-      "name": "Meme Genrator",
-      "category": "HTML, CSS, JS",
-      "img": "images/dispic2.jpeg"
     },
     {
       "link": "https://coderahman.github.io/sociallinkspage/",
@@ -36,34 +36,10 @@ const projects = ` [
       "img": "images/dispic7.png"
     },
     {
-      "link": "https://coderahman.github.io/web102_prework/",
-      "name": "CodePath PreWork",
-      "category": "HTML, CSS, JS",
-      "img": "images/dispic8.png"
-    },
-    {
-      "link": "https://southernmissgames.netlify.app/",
-      "name": "Southern Miss Games",
-      "category": "REACT",
-      "img": "images/dispic9.png"
-    },
-    {
       "link": "https://footballflashcards.netlify.app/",
       "name": "Football Flashcards",
       "category": "REACT",
       "img": "images/dispic10.png"
-    },
-    {
-      "link": "https://nasaastronomypictures.netlify.app/",
-      "name": "NASA Astronomy Pictures",
-      "category": "REACT",
-      "img": "images/dispic11.png"
-    },
-    {
-      "link": "https://nasaastronomypictures.netlify.app/",
-      "name": "FootyHub",
-      "category": "REACT",
-      "img": "images/dispic11.png"
     }
   ]
   `
